c1ed6835358f08a6923bf1e6ecd20cee694b40a6
Eugene_Kachanouski <cheryboom100@gmail.com>
2018-10-07 10:20:00 +0300

    Autoversioning and permissions done.

--- app/src/main/java/com/example/eugene_kachanouski/softwaredesignproject/MainActivity.java
+++ app/src/main/java/com/example/eugene_kachanouski/softwaredesignproject/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.eugene_kachanouski.softwaredesignproject;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 0;
+    private final int PERMISSIONS_REQUEST_READ_PHONE_STATE = 0;
     private String ALLOW_READ_PHONE_STATE;
+
+    private TextView imeiTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,49 +33,67 @@ public class MainActivity extends AppCompatActivity {
         String versionName = BuildConfig.VERSION_NAME;
         versionTextView.setText(versionName);
 
-        // Checking READ_PHONE_STATE permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            // Asking for permission
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE
-            );
+        imeiTextView = findViewById(R.id.imeiTextView);
+
+        getPhoneImei();
+
+    }
+
+    private void getPhoneImei()
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+                dialogBuilder.setMessage(ALLOW_READ_PHONE_STATE);
+                dialogBuilder.setTitle("Allow permissions!");
+                dialogBuilder.setCancelable(false);
+                dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.READ_PHONE_STATE},
+                                PERMISSIONS_REQUEST_READ_PHONE_STATE);
+                    }
+                });
+
+                dialogBuilder.show();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        PERMISSIONS_REQUEST_READ_PHONE_STATE);
+            }
+
         } else {
-            String IMEI = ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
-            setTextToIMEITextView(IMEI);
+            showImei();
         }
 
+    }
+
+    private void showImei()
+    {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)!=PackageManager.PERMISSION_GRANTED)
+            return;
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = tm.getDeviceId();
+        imeiTextView.setText(imei);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
-            // Processing permission results
-            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
-                String IMEI = ALLOW_READ_PHONE_STATE;
-
+            case PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+                // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    try {
-                        IMEI = ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
-                    } catch (SecurityException e) {
-
-                    }
+                    showImei();
+                } else {
+                    imeiTextView.setText(ALLOW_READ_PHONE_STATE);
                 }
-                setTextToIMEITextView(IMEI);
+                return;
             }
         }
-    }
-
-    /**
-     * Sets passed text to imeiTextView on layout
-     * @param text - text to be set
-     * @throws SecurityException
-     */
-    private void setTextToIMEITextView(String text) throws SecurityException {
-        TextView IMEITextView = findViewById(R.id.imeiTextView);
-        IMEITextView.setText(text);
     }
 }
