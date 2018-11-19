ea1c8448f09636427cc20176b2e6252455032f9d
Eugene_Kachanouski <cheryboom100@gmail.com>
2018-11-19 22:13:01 +0300

    Moved to AndroidX

--- app/src/main/java/com/example/eugene_kachanouski/softwaredesignproject/MainActivity.java
+++ app/src/main/java/com/example/eugene_kachanouski/softwaredesignproject/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.eugene_kachanouski.softwaredesignproject;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
